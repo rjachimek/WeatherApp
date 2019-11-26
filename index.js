001ad5fd2ebf68e8b3f4236bbd2c009fb8f56f62
Radek <225951@student.pwr.edu.pl>
2019-11-26 02:39:35 +0100

    Added newds api

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     let id = 0;
-    const key = "6435a094b1838feb1771367559ced5f4";
+    const openWeatherKey = "6435a094b1838feb1771367559ced5f4";
+    const newsKey = "da4b99413d7b47c8b7ce89892570c2ae";
 
     // function getCityId() {
 
@@ -30,14 +31,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function getWeather(cityName) {
 
-        fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&APPID=${key}`)
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&APPID=${openWeatherKey}&units=metric`)
             .then((resp) => resp.json())
             .then((data) => {
-                console.log(data)
+                console.log(data.list[0].main.temp);
             })
             .catch(err => {
                 console.log(err);
             })
+    }
+
+    function getNews() {
+
+        var req = new Request(`https://newsapi.org/v2/top-headlines?country=pl&apiKey=${newsKey}`);
+        fetch(req)
+            .then((resp) => resp.json())
+            .then((data) => {
+                let img = document.querySelectorAll('.news');
+                img[0].children[0].setAttribute("src", data.articles[0].urlToImage);
+                img[0].children[1].setAttribute("src", data.articles[1].urlToImage);
+                img[0].children[2].setAttribute("src", data.articles[2].urlToImage);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
     }
 
     document.querySelector('.search').addEventListener("submit", (e) => {
@@ -46,7 +64,6 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(cityName);
         getWeather(cityName);
     });
-    
 
-
+  
 })
